81740c949d7faf6cbf5a0d7fb357383dcabe3b75
Hamza Tanveer <hamzatan.ht@outlook.com>
2019-08-20 20:52:00 -0400

    Updated grab ID from API and created function to fetch price from API

--- main.py
+++ main.py
@@ -2,23 +2,35 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-    'start': '1',
-    'limit': '1',
-    'convert': 'USD'
-}
 headers = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': 'dac2a887-1990-486d-a476-dd8dcf037a61',
+    'X-CMC_PRO_API_KEY': 'dac2a887-1990-486d-a476-dd8dcf037a61'
 }
 
+tickerList = r'/home/htanveer/Documents/tickerList.txt'
+cryptoTicker = []
+
+with open(tickerList, 'r') as file:
+    cryptoTicker = file.read()
+
+print("CryptoPrice is running...")
+mapurl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+idurl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
 session = Session()
 session.headers.update(headers)
+idData = ''
 
 try:
-    response = session.get(url, params=parameters)
+    parameters = ''
+    for items in cryptoTicker:
+        parameters += ','.join(items)
+
+    response = session.get(mapurl, param=parameters)
     data = json.loads(response.text)
-    print(data)
+    for outdata in data[data]:
+        idData = ','.join(outdata)
+
+    response = session.get(idurl, param=parameters)
 except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
+    pass
+
